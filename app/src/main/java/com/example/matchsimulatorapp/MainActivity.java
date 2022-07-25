154bf40fb88dc3b05c1fc8c403c233b58f899616
EriqueRocha <eriquebit@gmail.com>
2022-07-25 18:00:57 -0300

    correção de link API

--- app/src/main/java/com/example/matchsimulatorapp/MainActivity.java
+++ app/src/main/java/com/example/matchsimulatorapp/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupHttpClient() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://digitalinnovationone.github.io/matches-simulator-api/")
+                .baseUrl("https://eriquerocha.github.io/simulator-api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         matchesApi = retrofit.create(MatchesAPI.class);
